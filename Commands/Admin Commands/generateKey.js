7ea7559212b1ba4b709cb32b8bb5b96003b78834
Brandon <134825293+senpai0807@users.noreply.github.com>
2023-05-28 09:58:12 -0400

    Update generateKey.js
    Updated to generate keys and store within the MongoDB

--- Commands/Admin Commands/generateKey.js
+++ Commands/Admin Commands/generateKey.js
@@ -1,13 +1,25 @@
-const { SlashCommandBuilder, ChatInputCommandInteraction } = require('discord.js');
-const fetch = require('node-fetch');
-const settings = require("../../Structures/config.json");
+const { SlashCommandBuilder, ChatInputCommandInteraction, PermissionFlagsBits, EmbedBuilder } = require('discord.js');
+const fs = require('fs');
+const config = require('../../Structures/config.json');
+const Key = require('../../Structures/Schemas/KeysDB');
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('generate')
     .setDescription('Generate a new key')
-    .addStringOption(option => option.setName('password').setDescription('Enter your password').setRequired(true)),
-
+    .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
+    .addStringOption(option => option.setName('password').setDescription('Enter your password').setRequired(true))
+    .addStringOption(option => 
+        option.setName('keytype')
+        .setDescription('Type of key')
+        .setRequired(true)
+        .addChoices(
+          { name: 'Weekly', value: 'weekly' },
+          { name: 'Monthly', value: 'monthly' },
+          { name: 'Renewal', value: 'renewal' },
+          { name: 'Lifetime', value: 'lifetime' }
+        )
+    ),
   /**
    * 
    * @param {ChatInputCommandInteraction} interaction 
@@ -16,33 +28,72 @@ module.exports = {
     const { options } = interaction;
     const user = interaction.user;
 
-    const password = options.getString('password');
-    const date = new Date();
-    const payload = {
-      password: password,
-      client: user.id,
-      date: date.toLocaleDateString(),
-    };
+    const serverData = JSON.parse(fs.readFileSync('serverInfo.json'));
+    const guildId = interaction.guildId;
+    const serverInfo = serverData[guildId];
 
-    fetch(`${settings.siteBase}/key/generate/lifetime`, {
-      headers: {
-        "content-type": "application/json",
-      },
-      referrerPolicy: "strict-origin-when-cross-origin",
-      body: JSON.stringify(payload),
-      method: "POST",
-      mode: "cors",
-    }).then((response) => {
-      response.json().then(async function (json) {
-        if (json["status"] === "Generated") {
-          await interaction.reply("Successfully generated key!");
-        } else {
-          await interaction.reply({ content: 'Something went wrong with the key generation.', ephemeral: true });
-        }
+    const password = options.getString('password');
+    
+    if (password !== config.adminPassword) {
+      await interaction.reply({ content: 'Wrong password.', ephemeral: true });
+      return;
+    }
+
+    const keytype = options.getString('keytype');
+
+    const date = new Date();
+    let expiryDate = new Date();
+
+    switch(keytype) {
+      case 'weekly':
+        expiryDate.setDate(date.getDate() + 7);
+        break;
+      case 'monthly':
+      case 'renewal':
+        expiryDate.setDate(date.getDate() + 30);
+        break;
+      case 'lifetime':
+        expiryDate = null;
+        break;
+      default:
+        await interaction.reply({ content: 'Invalid key type.', ephemeral: true });
+        return;
+    }
+
+    const key =
+      generate(6) + "-" + generate(6) + "-" + generate(6) + "-" + generate(6);
+
+      const newKey = new Key({
+        key,
+        created: date,
+        expires: expiryDate,
+        keyType: keytype,
+      });      
+
+    newKey.save()
+      .then(async () => {
+        const embed = new EmbedBuilder()
+          .setTitle('Successfully Generated Key 🌙')
+          .setColor(serverInfo.hexColor)
+          .setDescription(`Key: ${key}\nExpires: ${expiryDate}\nType: ${keytype}`)
+          .setThumbnail(serverInfo.thumbnail)
+          .setTimestamp()
+          .setFooter({ text: serverInfo.serverName, iconURL: serverInfo.thumbnail })
+        await interaction.reply({ embeds: [embed] });
+      })
+      .catch(async (err) => {
+        console.error(err);
+        await interaction.reply({ content: 'Something went wrong with the key generation.', ephemeral: true });
       });
-    }).catch(async (err) => {
-      console.error(err);
-      await interaction.reply({ content: 'Something went wrong with the key generation.', ephemeral: true });
-    });
   },
 };
+
+function generate(length) {
+  var result = "";
+  var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ12345678901234567890";
+  var charactersLength = characters.length;
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
