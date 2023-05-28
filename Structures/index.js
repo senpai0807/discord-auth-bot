3206bd68d05df9591ec93adbb579c55a25d248d0
Brandon <134825293+senpai0807@users.noreply.github.com>
2023-05-28 11:01:55 -0400

    Added routine check to delete unbinded keys after 24 hours.

--- Structures/index.js
+++ Structures/index.js
@@ -1,10 +1,11 @@
+const fs = require('fs');
 const { Client, GatewayIntentBits, Partials, Collection } = require('discord.js');
-const { Guilds, GuildMembers, GuildMessages, GuildMessageReactions, MessageContent, GuildVoiceStates } = GatewayIntentBits;
-const { User, Message, GuildMember, ThreadMember } = Partials;
+const { Guilds, GuildMembers, GuildMessages, GuildMessageReactions, MessageContent, GuildVoiceStates, DirectMessages, DirectMessageTyping } = GatewayIntentBits;
+const { User, Message, GuildMember, ThreadMember, Channel } = Partials;
 
 const client = new Client ({ 
-    intents: [ Guilds, GuildMembers, GuildMessages, GuildMessageReactions, MessageContent, GuildVoiceStates ],
-    partials: [ User, Message, GuildMember, ThreadMember ] 
+    intents: [ Guilds, GuildMembers, GuildMessages, GuildMessageReactions, MessageContent, GuildVoiceStates, DirectMessages, DirectMessageTyping ],
+    partials: [ User, Message, Channel, GuildMember, ThreadMember ] 
 });
 
 const { loadEvents } = require('./Handlers/eventHandler');
@@ -20,6 +21,39 @@ connect(client.config.DatabaseURL, {
 
 loadEvents(client);
 
+const Key = require('../Structures/Schemas/KeysDB');
+
+setInterval(async () => {
+    const currentDate = new Date();
+
+    const keys = await Key.find({
+        isBinded: false,
+        $or: [
+            { created: { $lte: new Date(currentDate.getTime() - 24 * 60 * 60 * 1000) } },
+            { unbindCooldown: { $lte: new Date(currentDate.getTime() - 24 * 60 * 60 * 1000) } }
+        ]
+    });
+
+    for (let key of keys) {
+        await key.remove();
+    }
+
+}, 12 * 60 * 60 * 1000);
+
+setInterval(async () => {
+    const keys = await Key.find({});
+
+    const formattedKeys = keys.map(key => ({
+        key: key.key,
+        created: key.created,
+        expires: key.expires,
+        isBinded: key.isBinded,
+        keyType: key.keyType
+    }));
+
+    fs.writeFileSync('keys.json', JSON.stringify(formattedKeys, null, 2));
+
+}, 60 * 60 * 1000);
 
 
 client.login(client.config.token);
