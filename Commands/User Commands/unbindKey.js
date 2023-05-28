76edeab1136e44ed07c97594c9f6394d54a4d44e
Brandon <134825293+senpai0807@users.noreply.github.com>
2023-05-28 09:59:00 -0400

    Update unbindKey.js

--- Commands/User Commands/unbindKey.js
+++ Commands/User Commands/unbindKey.js
@@ -1,6 +1,6 @@
 const { SlashCommandBuilder, ChatInputCommandInteraction, PermissionFlagsBits } = require('discord.js');
-const fetch = require('node-fetch');
-const settings = require("../../Structures/config.json");
+const Key = require('../../Structures/Schemas/KeysDB');
+const fs = require('fs');
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -8,7 +8,6 @@ module.exports = {
     .setDescription('Unbind the key')
     .setDefaultMemberPermissions(PermissionFlagsBits.UseApplicationCommands)
     .addStringOption(option => option.setName('key').setDescription('The key to unbind').setRequired(true)),
-
   /**
    * 
    * @param {ChatInputCommandInteraction} interaction 
@@ -16,32 +15,27 @@ module.exports = {
   async execute(interaction) {
     const user = interaction.user;
     const key = interaction.options.getString('key');
-    const date = new Date();
-    const payload = {
-      user: key,
-      client: user.id,
-      date: date.toLocaleDateString(),
-    };
 
-    fetch(`${settings.siteBase}/user/key/unbind`, {
-      headers: {
-        "content-type": "application/json",
-      },
-      referrerPolicy: "strict-origin-when-cross-origin",
-      body: JSON.stringify(payload),
-      method: "POST",
-      mode: "cors",
-    }).then((response) => {
-      response.json().then(async function (json) {
-        if (json["status"] === "unbinded") {
-          await interaction.reply("Key unbound");
-        } else {
-          await interaction.reply({ content: 'Something went wrong unbinding the key.', ephemeral: true });
-        }
-      });
-    }).catch(async (err) => {
-      console.error(err);
-      await interaction.reply({ content: 'Something went wrong unbinding the key.', ephemeral: true });
-    });
+    const keyDoc = await Key.findOne({ key: key });
+
+    if (!keyDoc || keyDoc.userId !== user.id) {
+      await interaction.reply({ content: 'This key does not exist or it is not bound to your account.', ephemeral: true });
+      return;
+    }
+
+    if (keyDoc.unbindCooldown && keyDoc.unbindCooldown > Date.now()) {
+      await interaction.reply({ content: 'This key cannot be unbound yet due to cooldown.', ephemeral: true });
+      return;
+    }
+
+    keyDoc.userId = null;
+
+    const cooldownDate = new Date();
+    cooldownDate.setDate(cooldownDate.getDate() + 30);
+    keyDoc.unbindCooldown = cooldownDate;
+
+    await keyDoc.save();
+
+    await interaction.reply("Key unbound");
   },
 };
