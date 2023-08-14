9d965b5996f96b6aa4e28f143d1c96de25f9d85b
Brandon <134825293+senpai0807@users.noreply.github.com>
2023-08-14 16:52:07 -0400

    Update index.js

--- Structures/index.js
+++ Structures/index.js
@@ -17,7 +17,12 @@ client.events = new Collection();
 
 const { connect } = require('mongoose');
 connect(client.config.DatabaseURL, {
-}).then(() => console.log('The client is now connected to the database.'));
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    bufferCommands: false
+  })
+  .then(() => console.log('The client is now connected to the database.'))
+  .catch(err => console.error('An error occurred while connecting to the database:', err));
 
 loadEvents(client);
 
@@ -35,7 +40,9 @@ setInterval(async () => {
     });
 
     for (let key of keys) {
-        await key.remove();
+        if (key && typeof key.remove === 'function') {
+            await key.remove();
+        }
     }
 
 }, 12 * 60 * 60 * 1000);
@@ -48,12 +55,13 @@ setInterval(async () => {
         created: key.created,
         expires: key.expires,
         isBinded: key.isBinded,
-        keyType: key.keyType
+        keyType: key.keyType,
+        blacklist: key.blacklist,
     }));
 
     fs.writeFileSync('keys.json', JSON.stringify(formattedKeys, null, 2));
 
-}, 60 * 60 * 1000);
+}, 60 * 1000);
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -68,38 +76,66 @@ app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+let blacklist = [];
+
+app.post('/blacklist', async (req, res) => {
+    const { licenseKey, ip, deviceName } = req.body;
+
+    const key = await Key.findOne({ key: licenseKey });
+    if (key) {
+        key.blacklist = true;
+        await key.save();
+    }
+
+    blacklist.push({ licenseKey, ip, deviceName });
+    res.json({ message: 'User has been blacklisted.' });
+});
+
 app.post('/auth', async (req, res) => {
-    const { licenseKey, deviceName } = req.body;
-        console.log(`Received license key: ${licenseKey} from device: ${deviceName}`);
+    const { licenseKey, deviceName, deviceID, ip } = req.body;
+    console.log(`Received license key: ${licenseKey} from device: ${deviceName}`);
 
     const userKey = await Key.findOne({ key: licenseKey });
-        console.log(`Found userKey in database: ${JSON.stringify(userKey)}`);
 
     if (!userKey) {
-        return res.status(401).json({ message: 'Authentication failed: Invalid discordId or licenseKey.' });
+        return res.status(401).json({ message: 'InvalidLicenseKey' });
+      }
+
+    if (userKey.blacklist) {
+        return res.status(401).json({ message: 'This user is blacklisted.' });
     }
 
     if (userKey.expires < new Date()) {
         return res.status(401).send('Authentication failed: License key has expired.');
     }
 
+    const currentDevice = { deviceID, ip };
+
     if (userKey.activeDevices.length >= 2) {
-        return res.status(401).send('Authentication failed: Maximum number of devices using this license key has been reached.');
+        for(let device of userKey.activeDevices) {
+            if(device.deviceID === deviceID && device.ip === ip) {
+                const user = await client.users.fetch(userKey.userId);
+
+                return res.json({
+                    message: 'Authentication successful.',
+                    discordId: userKey.userId,
+                    discordName: user.username,
+                });
+            }
+        }
+        return res.status(401).json({ message: 'MaxInstancesReached' });
     }
 
+    userKey.activeDevices.push(currentDevice);
+    await userKey.save();
 
-    if (!userKey.activeDevices.includes(deviceName)) {
-        userKey.activeDevices.push(deviceName);
-        await userKey.save();
-    }
-    
     const user = await client.users.fetch(userKey.userId);
 
     res.json({
         message: 'Authentication successful.',
         discordId: userKey.userId,
         discordName: user.username,
-      });
+    });
 });
 
 app.listen(port, () => console.log(`Server is running on port ${port}.`));
